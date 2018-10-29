296e258cb54163f16c513fd8e91d8baa085214b4
Dmirtiy <stadnik.di@gmail.com>
2018-10-29 16:39:50 +0200

    Finished all resolution

--- js/index.js
+++ js/index.js
@@ -14,6 +14,25 @@ $(window).ready(function(){
                 }
             },
             {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {
                 breakpoint: 576,
                 settings:{
                     slidesToShow: 2,
@@ -45,6 +64,20 @@ $(window).ready(function(){
                 }
             },
             {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint : 768,
+                settings:{
+                    slidesToShow:1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
                 breakpoint: 576,
                 settings:{
                     slidesToShow: 1,
@@ -73,6 +106,18 @@ $(window).ready(function(){
                 breakpoint: 1600,
                 settings:{
                     slidesToShow:2,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings:{
+                    slidesToShow:1,
+                }
+            },
+            {
+                breakpoint : 768,
+                settings:{
+                    slidesToShow:1,
                 }
             },
             {
